fad956b553aab1720c283d2a91d3c378ca2b8354
spamegg <spamegg@pm.me>
2024-10-07 23:44:38 +0300

    progress on grammar

--- Sentence.scala
+++ Sentence.scala
@@ -1,29 +1,49 @@
 package tarski
 
-enum Logical:
-  case AND, OR, NOT, IMPLIES, BICOND
-import Logical.*
-
-enum Operator:
-  case EQ, NEQ
-import Operator.*
-
-enum Quantifier:
-  case Forall, Exists
-import Quantifier.*
-
 enum Variable:
   case S, T, U, V, W, X, Y, Z
-import Variable.*
 
 enum Name:
   case A, B, C, D, E, F, G, H
-import Name.*
 
-enum Paren:
-  case L, R
-import Paren.*
+type Term = Variable | Name
 
-type Token = Logical | Operator | Quantifier | Variable | Name | Paren
+enum Atomic:
+  case Small(t: Term)
+  case Medium(t: Term)
+  case Large(t: Term)
+  case Circle(t: Term)
+  case Triangle(t: Term)
+  case Square(t: Term)
+  case Blue(t: Term)
+  case Black(t: Term)
+  case Gray(t: Term)
+  case LeftOf(t1: Term, t2: Term)
+  case RightOf(t1: Term, t2: Term)
+  case FrontOf(t1: Term, t2: Term)
+  case BackOf(t1: Term, t2: Term)
+  case Adjoins(t1: Term, t2: Term)
+  case Smaller(t1: Term, t2: Term)
+  case Larger(t1: Term, t2: Term)
+  case Same(t1: Term, t2: Term)
+  case SameSize(t1: Term, t2: Term)
+  case SameShape(t1: Term, t2: Term)
+  case SameColor(t1: Term, t2: Term)
+  case SameRow(t1: Term, t2: Term)
+  case SameCol(t1: Term, t2: Term)
+  case Between(t1: Term, t2: Term, t3: Term)
 
-case class Sentence(stuff: Int)
+enum Formula:
+  case Atom(a: Atomic)
+  case Not(f: Formula)
+  case And(f1: Formula, f2: Formula)
+  case Or(f1: Formula, f2: Formula)
+  case Implies(f1: Formula, f2: Formula)
+  case Bicond(f1: Formula, f2: Formula)
+  case Forall(v: Variable, f: Formula)
+  case Exists(v: Variable, f: Formula)
+
+  def hasNoFreeVars = true
+
+case class Sentence(f: Formula):
+  require(f.hasNoFreeVars)
