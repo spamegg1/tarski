8abb7db804ebd46aa4a02dac1d49c91f7c6454eb
spamegg <spamegg@pm.me>
2024-10-08 18:24:44 +0300

    start using property based testing in atomic formulas

--- test/Generators.scala
+++ test/Generators.scala
@@ -1,8 +1,24 @@
 package tarski
 package testing
 
-import org.scalacheck.{Gen, Test, Prop}, Prop.forAll
+import org.scalacheck.{Gen, Test, Prop}
 
 val varGen = Gen.oneOf[Var](Var.values)
 val nameGen = Gen.oneOf[Name](Name.values)
 val termGen = Gen.oneOf[Term](varGen, nameGen)
+
+val atomFreeGen =
+  for
+    vari <- varGen
+    t1 <- termGen
+    t2 <- termGen
+  yield (vari, Seq(Medium(vari), LeftOf(vari, t1), Between(vari, t1, t2)))
+
+val atomNonFreeGen =
+  for
+    vari <- varGen
+    t1 <- termGen
+    t2 <- termGen
+    t3 <- termGen
+    if vari != t1 && vari != t2 && vari != t3
+  yield (vari, Seq(Medium(t1), LeftOf(t1, t2), Between(t1, t2, t3)))
