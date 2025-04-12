9e28af2d8b781116df5e62f407d31dbf949865c5
spamegg <spamegg@pm.me>
2025-04-12 15:27:45 +0300

    update toolkit and doodle

--- project.scala
+++ project.scala
@@ -1,4 +1,4 @@
 //> using scala 3.6.4
-//> using toolkit 0.6.0
-//> using dep org.creativescala::doodle:0.26.0
+//> using toolkit 0.7.0
+//> using dep org.creativescala::doodle:0.28.0
 //> using test.dep org.scalacheck::scalacheck:1.18.1
