dcf7e760e1e229fe6c780e33197ac8946920d11e
spamegg <spamegg@pm.me>
2025-09-01 23:22:22 +0300

    fix compile and test errors

--- view/Constants.scala
+++ view/Constants.scala
@@ -9,18 +9,18 @@ val Title    = "Tarski's World"
 val TickRate = FiniteDuration(50L, "milliseconds")
 
 // derived constants
-val Height = Size * 8
-val Width  = Size * 16
-val Small  = Size * 0.4
-val Medium = Size * 0.7
-val Large  = Size * 0.95
+val Height    = Size * 8
+val Width     = Size * 16
+val SmallSize = Size * 0.4
+val MidSize   = Size * 0.7
+val LargeSize = Size * 0.95
 
 // basic shapes
-val Square  = Image.square(Size)
-val WhiteSq = Square.fillColor(white)
-val BlackSq = Square.fillColor(black)
-val Wb      = WhiteSq beside BlackSq
-val Bw      = BlackSq beside WhiteSq
+val Sqr   = Image.square(Size)
+val WhSqr = Sqr.fillColor(white)
+val BlSqr = Sqr.fillColor(black)
+val Wb    = WhSqr beside BlSqr
+val Bw    = BlSqr beside WhSqr
 
 // derived shapes
 val Line1   = Wb beside Wb beside Wb beside Wb
