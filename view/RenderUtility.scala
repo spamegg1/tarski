d89526f7de7a95fd25f424f2c5ce353dc0a7c950
spamegg <spamegg@pm.me>
2025-10-05 14:11:51 +0300

    done rendering controls: small refactor

---
+++ view/RenderUtility.scala
@@ -0,0 +1,16 @@
+package tarski
+package view
+
+val (wid, hgh) = (ControlsConverter.blockWidth, ControlsConverter.blockHeight)
+def button(factor: Int = 1) = Image.rectangle(wid * factor, hgh).fillColor(Gray)
+
+def renderIndicator(point: Point, factor: Int = 1) = Image
+  .rectangle(wid * factor, hgh)
+  .strokeColor(red)
+  .strokeWidth(SmallStroke)
+  .at(point)
+
+def renderButton(name: String, point: Point, factor: Int = 1) = Text(name)
+  .font(TheFont)
+  .on(button(factor))
+  .at(point)
--- view/Renderer.scala
+++ view/Renderer.scala
@@ -1,20 +1,6 @@
 package tarski
 package view
 
-val (wid, hgh) = (ControlsConverter.blockWidth, ControlsConverter.blockHeight)
-def button(factor: Int = 1) = Image.rectangle(wid * factor, hgh).fillColor(Gray)
-
-def renderIndicator(point: Point, factor: Int = 1) = Image
-  .rectangle(wid * factor, hgh)
-  .strokeColor(red)
-  .strokeWidth(SmallStroke)
-  .at(point)
-
-def renderButton(name: String, point: Point, factor: Int = 1) = Text(name)
-  .font(TheFont)
-  .on(button(factor))
-  .at(point)
-
 val renderEval = renderButton("Eval", evalPt, 2)
 val renderMove = renderButton("Move", movePt, 2)
 val renderAdd  = renderButton("Add", addPt, 2)
