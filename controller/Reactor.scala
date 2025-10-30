617dbbb438791fe4f229fe6be16eae535f753639
spamegg <spamegg@pm.me>
2025-10-30 21:28:41 +0300

    factor out handlers

--- controller/Reactor.scala
+++ controller/Reactor.scala
@@ -1,8 +1,6 @@
 package tarski
 package controller
 
-import Status.*, Result.*, Shape.*
-
 def click(p: Point, world: World): World =
   if p.x < 0 then
     val pos = BoardConverter.toPos((p - BoardOrigin).toPoint)
@@ -11,57 +9,6 @@ def click(p: Point, world: World): World =
     val pos = ControlsConverter.toPos((p - ControlsOrigin).toPoint)
     handleControls(pos, world)
   else world // do nothing for now
-
-def handlePos(pos: Pos, world: World): World =
-  if world.controls.move then // make sure move is enabled
-    world.controls.pos match
-      case Some(p) => world.moveBlock(from = p, to = pos)
-      case None    => world
-  else // move is disabled, select another pos or de-select current
-    world.controls.pos match
-      case Some(p) if pos == p => world.copy(controls = world.controls.deselectPos)
-      case _                   => world.copy(controls = world.controls.selectPos(pos))
-
-def handleControls(pos: Pos, world: World): World = gridControl.get(pos) match
-  case None => world
-  case Some(value) => // make sure a button is clicked
-    value match
-      case "a" | "b" | "c" | "d" | "e" | "f" => handleName(value, world)
-      case "Eval" =>
-        val results = world.formulas.map: (formula, result) =>
-          val bool = eval(formula)(using world.blocks)
-          formula -> (if bool then Valid else Invalid)
-        world.copy(formulas = results)
-      case "Add" =>
-        Block.fromControls(world.controls) match
-          case None => world
-          case Some(block) => // make sure a block can be created
-            world.controls.pos match
-              case None    => world
-              case Some(p) => world.addBlockAt(p, block)
-      case "Del"   => world.removeBlockAt(pos)
-      case "Move"  => world.copy(controls = world.controls.toggleMove)
-      case "Blue"  => world.copy(controls = world.controls.setColor(Blue))
-      case "Green" => world.copy(controls = world.controls.setColor(Green))
-      case "Gray"  => world.copy(controls = world.controls.setColor(Gray))
-      case "Small" => world.copy(controls = world.controls.setSize(Small))
-      case "Mid"   => world.copy(controls = world.controls.setSize(Medium))
-      case "Large" => world.copy(controls = world.controls.setSize(Large))
-      case "Tri"   => world.copy(controls = world.controls.setShape(Tri))
-      case "Squ"   => world.copy(controls = world.controls.setShape(Squ))
-      case "Cir"   => world.copy(controls = world.controls.setShape(Cir))
-      case "Block" => world
-
-def handleName(name: String, world: World): World = world.names.get(name) match
-  case None => world // name does not exist, do nothing
-  case Some(Available) => // name is available, attempt to add name to selected block
-    world.controls.pos match
-      case None      => world // no block is selected, do nothing
-      case Some(pos) => world.addNameToBlockAt(pos, name)
-  case Some(Occupied) => // name is occupied, attempt to remove name from its block
-    world.blocks.get(name) match
-      case None           => world // there is no block with that name, should not happen!
-      case Some((_, pos)) => world.removeNameFromBlockAt(pos)
 
 // These do nothing; the world is static except for clicking controls.
 def tick(world: World): World        = world
