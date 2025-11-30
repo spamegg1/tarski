aaf23fad13a9ea74b2fd21a0cc9a5bc7cb4e1862
spamegg <spamegg@pm.me>
2025-11-30 10:44:24 +0300

    update Github actions and add using directives for publishing

--- project.scala
+++ project.scala
@@ -3,3 +3,17 @@
 //> using dep org.creativescala::doodle:0.32.0
 //> using dep at.logic.gapt::gapt:2.18.1
 //> using option -explain-cyclic
+
+//> using publish.organization io.github.spamegg1
+//> using publish.name tarski
+//> using publish.license Apache-2.0
+//> using publish.computeVersion git:tag
+//> using publish.developers "spamegg1|Spamegg|https://github.com/spamegg1"
+//> using publish.url https://github.com/spamegg1/tarski
+//> using publish.vcs github:spamegg1/tarski
+//> using publish.repository central
+//> using publish.user env:SONATYPE_USERNAME
+//> using publish.password env:SONATYPE_PASSWORD
+//> using publish.gpgKey A467BFB403160D9F
+//> using publish.secretKey env:PGP_SECRET
+//> using publish.secretKeyPassword env:PGP_PASSPHRASE
