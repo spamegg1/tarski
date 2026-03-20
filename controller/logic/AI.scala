839cf9155c3f810749e28fc97ec2ea9751525196
spamegg <spamegg@pm.me>
2026-03-20 14:45:04 +0300

    refactor computer's choice messages for And/Or

--- controller/logic/AI.scala
+++ controller/logic/AI.scala
@@ -1,7 +1,7 @@
 package tarski
 package controller
 
-/** Makes the block choices of the computer player in the game. */
+/** Makes various choices and generates messages for the computer player in the game. */
 object AI:
   /** Chooses the name of a block in the world that satisfies, or falsifies, the given formula by substituting the name
     * for the given variable.
@@ -23,3 +23,21 @@ object AI:
       .find(pred || !_._2) match
       case None            => nm.keys.head
       case Some((name, _)) => name
+
+  /** Generates messages for the computer's formula choices in case of `And` and `Or`.
+    *
+    * @param a
+    *   The first conjunct / disjunct.
+    * @param b
+    *   The second conjunct / disjunct.
+    * @param commit
+    *   The computer's commitment (the negation of the player's commitment).
+    * @return
+    *   A list of messages for the computer's choice from the two formulas.
+    */
+  def chooseAndOr(a: FOLFormula, b: FOLFormula)(commit: Boolean)(using NameMap): Messages =
+    val choice = if Interpreter.eval(a) == commit then a else b
+    val msg1   = s"You believe both are ${!commit}:"
+    val msg2   = ui"$a and $b"
+    val msg3   = ui"I choose $choice as $commit."
+    msg1 :: msg2 :: msg3 :: Nil
--- controller/logic/Messager.scala
+++ controller/logic/Messager.scala
@@ -14,11 +14,17 @@ object Messager:
     */
   def show(play: Play)(using nm: NameMap): Messages =
     (play.commitment, play.formula) match
-      case (Some(false), And(a, b)) => chooseAndOr(a, b)(false)
-      case (Some(true), Or(a, b))   => chooseAndOr(a, b)(true)
-      case (Some(false), All(x, f)) => chooseAllEx(x, f)(false)
-      case (Some(true), Ex(x, f))   => chooseAllEx(x, f)(true)
+      // symmetric cases
+      case (Some(false), And(a, b))               => chooseAndOr(a, b)(false)
+      case (Some(true), Or(a, b))                 => chooseAndOr(a, b)(true)
+      case (Some(false), All(x, f))               => chooseAllEx(x, f)(false)
+      case (Some(true), Ex(x, f))                 => chooseAllEx(x, f)(true)
+      case (_, Imp(a, b))                         => rewrite(play.formula, Or(Neg(a), b))
+      case (_, Iff(a: FOLFormula, b: FOLFormula)) => rewrite(play.formula, And(Imp(a, b), Imp(b, a)))
+      case (Some(true), And(a, b))                => AI.chooseAndOr(a, b)(false)
+      case (Some(false), Or(a, b))                => AI.chooseAndOr(a, b)(true)
 
+      // outlier cases
       case (Some(commit), Neg(a)) =>
         val msg1 = s"You believe this is $commit:"
         val msg2 = ui"${Neg(a)}"
@@ -26,37 +32,11 @@ object Messager:
         val msg4 = ui"$a"
         msg1 :: msg2 :: msg3 :: msg4 :: Nil
 
-      case (_, Imp(a, b)) =>
-        val msg1 = ui"${play.formula} can be written as:"
-        val msg2 = ui"${Or(Neg(a), b)}"
-        msg1 :: msg2 :: Nil
-
-      case (_, Iff(a: FOLFormula, b: FOLFormula)) =>
-        val msg1 = ui"${play.formula} can be written as:"
-        val msg2 = ui"${And(Imp(a, b), Imp(b, a))}"
-        msg1 :: msg2 :: Nil
-
-      // Cases that need Interpreter and NameMap
       case (Some(commit), a: FOLAtom) =>
         val result = Interpreter.eval(a)
         val msg1   = if commit == result then "You win!" else "You lose."
-        val msg2   = ui"$a" + s" is $result in this world."
+        val msg2   = ui"$a is $result in this world."
         msg1 :: msg2 :: Nil
-
-      // combine these two
-      case (Some(true), And(a, b)) =>
-        val choice = if Interpreter.eval(a) then b else a // choose FALSE formula
-        val msg1   = "You believe both are true:"
-        val msg2   = ui"$a and $b"
-        val msg3   = ui"I choose $choice as false."
-        msg1 :: msg2 :: msg3 :: Nil
-
-      case (Some(false), Or(a, b)) =>
-        val choice = if Interpreter.eval(a) then a else b // choose TRUE formula
-        val msg1   = "You believe both are false:"
-        val msg2   = ui"$a and $b"
-        val msg3   = ui"I choose $choice as true."
-        msg1 :: msg2 :: msg3 :: Nil
 
       // combine these two
       case (Some(true), All(x, f)) =>
@@ -111,15 +91,20 @@ object Messager:
     val msg3   = "Click on a block, then click OK"
     msg1 :: msg2 :: msg3 :: Nil
 
-  /** Custom string interpolator to be used with `FOLFormula` in order to avoid `.toUntypedString` calls everywhere.
-    *
-    * @param args
-    *   The arguments to be interpolated.
-    * @return
-    *   The interpolated combined string of the arguments.
-    */
-  extension (sc: StringContext) def ui(args: String*): String = sc.s(args*)
-
-  /** This conversion enables us to utilize `.toUntypedString` in the [[ui]] custom interpolator. */
-  given Conversion[FOLFormula, String] = _.toUntypedString
+  private def rewrite(f: FOLFormula, g: FOLFormula): Messages =
+    ui"$f can be written as:" :: ui"$g" :: Nil
 end Messager
+
+/** Custom string interpolator to be used with `FOLFormula` in order to avoid `.toUntypedString` calls everywhere.
+  *
+  * @param args
+  *   The arguments to be interpolated.
+  * @return
+  *   The interpolated combined string of the arguments.
+  */
+extension (sc: StringContext) def ui(args: String*): String = sc.s(args*)
+
+/** This conversion enables us to utilize `.toUntypedString` in the [[ui]] custom interpolator. */
+given Conversion[FOLFormula, String] = _.toUntypedString
+
+given Conversion[AnyVal, String] = _.toString
