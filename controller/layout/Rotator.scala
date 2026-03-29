d582c0df5a3ddfa39d90d64cc1bd0c160b680736
spamegg <spamegg@pm.me>
2026-03-29 23:27:07 +0300

    fix Rotator by swapping rows and cols correctly after rotation

--- controller/layout/Rotator.scala
+++ controller/layout/Rotator.scala
@@ -4,10 +4,13 @@ package controller
 /** Class to rotate any given grid's positions 90 degrees clockwise or counter-clockwise.
   *
   * @param gs
-  *   A [[GridSize]] instance; the number of rows and columns of the grid we want to rotate.
+  *   A [[GridSize]] instance; the number of rows and columns of the grid we want to rotate. After a rotation, rows and
+  *   columns swap, so the numbers also have to be swapped.
   */
-case class Rotator(gs: GridSize):
+case class Rotator(var gs: GridSize):
   /** Rotates the given position 90 degrees, based on the given rotation.
+    *
+    * Has the side effect of swapping the numbers of rows and columns of `gs`.
     *
     * @param dir
     *   `Rotation.Left` (90 degrees counter-clockwise) or `Rotation.Right` (90 degrees clockwise).
@@ -16,9 +19,12 @@ case class Rotator(gs: GridSize):
     * @return
     *   The integer positions of the point obtained by rotating the grid 90 degrees clockwise or counter-clockwise.
     */
-  def rotate(dir: Rotation)(pos: Pos): Pos = dir match
-    case Rotation.Left  => (row = gs.cols - pos.col - 1, col = pos.row)
-    case Rotation.Right => (row = pos.col, col = gs.rows - pos.row - 1)
+  def rotate(dir: Rotation)(pos: Pos): Pos =
+    val oldGs = gs // save current row,col sizes for this rotation
+    gs = (rows = gs.cols, cols = gs.rows) // swap row and col sizes for next rotation
+    dir match
+      case Rotation.Left  => (row = oldGs.cols - pos.col - 1, col = pos.row)
+      case Rotation.Right => (row = pos.col, col = oldGs.rows - pos.row - 1)
 end Rotator
 
 /** Contains a useful instance of [[Rotator]]. */
