9917cadc5cef2ddd0c1e3aded1e2223552eb5a9c
spamegg <spamegg@pm.me>
2026-03-30 13:19:39 +0300

    add doc comment to Select

--- model/game/controls/Select.scala
+++ model/game/controls/Select.scala
@@ -1,7 +1,12 @@
 package tarski
 package model
 
-/** TODO
+/** Enum for the state of selecting a position on the board.
+  *
+  * If the user is not being prompted to select a block, then it's `Off`. When user is prompted and we are awaiting
+  * user's input, it's `Wait`. When a `pos` on the board is selected, it's `On(pos)`.
+  *
+  * It is parametric, so it could be potentially used for other selection states.
   */
 enum Select[+T]:
   case Off
