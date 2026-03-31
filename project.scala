a8eebddceca37a2df7d0ddab6e15167118510c37
spamegg <spamegg@pm.me>
2026-04-01 01:23:02 +0300

    bump Scala 3.8.3

--- project.scala
+++ project.scala
@@ -1,9 +1,9 @@
-//> using scala 3.8.3-RC3
+//> using scala 3.8.3
 //> using toolkit 0.9.1
 //> using resourceDir ./font
 //> using dep org.creativescala::doodle:0.34.0
 //> using dep at.logic.gapt::gapt:2.19.0
-//> using options -explain-cyclic -opt
+//> using options -explain-cyclic -opt -opt-inline:**
 
 //> using publish.organization io.github.spamegg1
 //> using publish.name tarski
